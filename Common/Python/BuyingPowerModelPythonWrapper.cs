8989178347fba7abe8582b4a825efcafa81e83ec
Michael Handschuh <mhandschuh@gmail.com>
2020-12-29 02:19:28 -0500

    WIP: Requires more unit tests and regression test

--- Common/Python/BuyingPowerModelPythonWrapper.cs
+++ Common/Python/BuyingPowerModelPythonWrapper.cs
@@ -1,4 +1,4 @@
-﻿/*
+/*
  * QUANTCONNECT.COM - Democratizing Finance, Empowering Individuals.
  * Lean Algorithmic Trading Engine v2.0. Copyright 2014 QuantConnect Corporation.
  *
@@ -48,7 +48,7 @@ namespace QuantConnect.Python
         /// <summary>
         /// Gets the buying power available for a trade
         /// </summary>
-        /// <param name="parameters">A parameters object containing the algorithm's potrfolio, security, and order direction</param>
+        /// <param name="parameters">A parameters object containing the algorithm's portfolio, security, and order direction</param>
         /// <returns>The buying power available for the trade</returns>
         public BuyingPower GetBuyingPower(BuyingPowerParameters parameters)
         {
